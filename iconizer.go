416f1e10ec790e061274bbc7cac0d25bf3f4d33f
ElectricR <chulkovroma@mail.ru>
2022-03-28 14:09:22 +0300

    Cpu stuff

--- iconizer.go
+++ iconizer.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 )
 
-func print(r string, value int) {
+func printBattery(r string, value int) {
 	fmt.Printf("%s%d%%", r, value)
 }
 
-func main() {
+func handleBattery() {
 	var charge int
 	_, err := fmt.Scan(&charge)
 	if err != nil {
@@ -17,24 +20,76 @@ func main() {
 	}
 	switch {
 	case charge < 10:
-		print("", charge)
+		printBattery("", charge)
 	case charge < 20:
-		print("", charge)
+		printBattery("", charge)
 	case charge < 30:
-		print("", charge)
+		printBattery("", charge)
 	case charge < 40:
-		print("", charge)
+		printBattery("", charge)
 	case charge < 50:
-		print("", charge)
+		printBattery("", charge)
 	case charge < 60:
-		print("", charge)
+		printBattery("", charge)
 	case charge < 70:
-		print("", charge)
+		printBattery("", charge)
 	case charge < 80:
-		print("", charge)
+		printBattery("", charge)
 	case charge < 90:
-		print("", charge)
+		printBattery("", charge)
 	default:
-		print("", charge)
+		printBattery("", charge)
+	}
+}
+
+func handleBluetooth() {
+	reader := bufio.NewReader(os.Stdin)
+	word, _ := reader.ReadString(' ')
+	switch string(word) {
+	case "no\n":
+		fmt.Print("")
+	case "yes\n":
+		fmt.Print("")
+	}
+}
+
+func handleCPU() {
+	stat, err := os.Open("/proc/stat")
+	if err != nil {
+		fmt.Print("0")
+		return
+	}
+	defer stat.Close()
+	reader := bufio.NewReader(stat)
+	var (
+		user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice int
+	)
+	fmt.Fscanf(reader, "cpu  %d %d %d %d %d %d %d %d %d", &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest, &guest_nice)
+
+	total := float64(user + nice + system + idle + iowait + irq + softirq + steal + guest + guest_nice)
+	var prevtotal, previdle float64
+
+	prev, err := os.Open("/tmp/awesome_iconizer_cpu")
+	if err == nil {
+		fmt.Fscanf(bufio.NewReader(prev), "%f %f", &prevtotal, &previdle)
+		prev.Close()
+	}
+	fmt.Print((1 - (float64(idle)-previdle)/(total-prevtotal)))
+
+	save, err := os.Create("/tmp/awesome_iconizer_cpu")
+	defer save.Close()
+	save.WriteString(fmt.Sprintf("%f %f", total, float64(idle)))
+}
+
+func main() {
+	mode := flag.String("mode", "", "")
+	flag.Parse()
+	switch *mode {
+	case "battery":
+		handleBattery()
+	case "bluetooth":
+		handleBluetooth()
+	case "cpu":
+		handleCPU()
 	}
 }
